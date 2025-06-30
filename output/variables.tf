b0cb6121d8e138396689817d9bc9e87424d15280
HarshPorwal-Aviato <harsh@aviato.consulting>
2025-06-30 13:26:37 +0530

    Initial commit

--- output/variables.tf
+++ output/variables.tf
@@ -1,83 +1,31 @@
 variable "project_id" {
   type        = string
-  description = "The ID of the project in which to operate."
+  description = "The ID of the project"
   default     = "aviato-game-fight-rvxirf"
 }
 
-variable "default_ssh_source_ranges" {
+variable "region" {
+  type        = string
+  description = "The region to deploy resources to"
+  default     = "us-central1"
+}
+
+variable "default_allowed_ssh_source_ranges" {
   type = list(string)
   default = [
     "10.0.0.0/8",
     "172.16.0.0/12",
     "192.168.0.0/16"
   ]
-  description = "Source ranges for SSH traffic"
+  description = "The allowed source ranges for SSH access"
 }
 
-variable "default_rdp_source_ranges" {
+variable "default_allowed_rdp_source_ranges" {
   type = list(string)
   default = [
     "10.0.0.0/8",
     "172.16.0.0/12",
     "192.168.0.0/16"
   ]
-  description = "Source ranges for RDP traffic"
-}
-
-variable "gcs_buckets_uniform_access" {
-  type = list(string)
-  default = [
-    "aviato-game-fight-rvxirf.appspot.com",
-    "aviato-game-fight-rvxirf_bucket",
-    "staging.aviato-game-fight-rvxirf.appspot.com"
-  ]
-  description = "List of GCS bucket names to enable uniform bucket level access"
-}
-
-variable "all_regions" {
-  type = list(string)
-  default = [
-    "asia-east1",
-    "asia-east2",
-    "asia-northeast1",
-    "asia-northeast2",
-    "asia-northeast3",
-    "asia-south1",
-    "asia-south2",
-    "asia-southeast1",
-    "asia-southeast2",
-    "australia-southeast1",
-    "australia-southeast2",
-    "europe-central2",
-    "europe-north1",
-    "europe-north2",
-    "europe-southwest1",
-    "europe-west1",
-    "europe-west2",
-    "europe-west3",
-    "europe-west4",
-    "europe-west6",
-    "europe-west8",
-    "europe-west9",
-    "europe-west10",
-    "me-central1",
-    "me-central2",
-    "me-west1",
-    "northamerica-northeast1",
-    "northamerica-northeast2",
-    "northamerica-south1",
-    "southamerica-east1",
-    "southamerica-west1",
-    "us-central1",
-    "us-east1",
-    "us-east4",
-    "us-east5",
-    "us-south1",
-    "us-west1",
-    "us-west2",
-    "us-west3",
-    "us-west4",
-    "africa-south1"
-  ]
-  description = "All GCP regions"
+  description = "The allowed source ranges for RDP access"
 }
