9a8405b7de9bf45b205e50b4cab8708218ffbc5b
HarshPorwal-Aviato <harsh@aviato.consulting>
2025-07-07 18:23:30 +0530

    Initial commit

--- output\storage.tf
+++ output\storage.tf
@@ -1,12 +1,7 @@
-resource "google_storage_bucket" "default_buckets" {
-  for_each = toset([
-    "aviato-game-fight-rvxirf.appspot.com",
-    "aviato-game-fight-rvxirf_bucket",
-    "staging.aviato-game-fight-rvxirf.appspot.com"
-  ])
-  name          = each.value
-  project       = var.project_id
-  location      = "US"
-  force_destroy = false
+resource "google_storage_bucket" "buckets_uniform_access" {
+  for_each = toset(var.uniform_bucket_level_access_buckets)
+  name     = each.value
+  project  = var.project_id
+  location = "US"
   uniform_bucket_level_access = true
 }
