5a6ff8370d1c7be996976e158bf71899f89c1bfd
HarshPorwal-Aviato <harsh@aviato.consulting>
2025-07-07 16:38:14 +0530

    Initial commit

--- output\compute.tf
+++ output\compute.tf
@@ -1,24 +1,31 @@
 resource "google_compute_network" "default" {
+  provider = google.gcp
   name                    = "default"
   project                 = var.project_id
-  delete_default_routes = true
+  auto_create_subnetworks = false
+
+  lifecycle {
+    ignore_changes = [
+      auto_create_subnetworks,
+    ]
+  }
 }
 
-resource "google_compute_firewall" "default_allow_rdp" {
+resource "google_compute_firewall" "rdp" {
+  provider = google.gcp
   name    = "default-allow-rdp"
   project = var.project_id
   network = "default"
-
   allow {
     protocol = "tcp"
     ports    = ["3389"]
   }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["rdp"]
+  direction     = "INGRESS"
+  source_ranges = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
 }
 
-resource "google_compute_firewall" "default_allow_ssh" {
+resource "google_compute_firewall" "ssh" {
+  provider = google.gcp
   name    = "default-allow-ssh"
   project = var.project_id
   network = "default"
@@ -28,18 +35,28 @@ resource "google_compute_firewall" "default_allow_ssh" {
     ports    = ["22"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh"]
+  direction     = "INGRESS"
+  source_ranges = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+}
+
+resource "google_compute_network_dns_logging_policy" "default" {
+  provider = google.gcp
+  name        = "default"
+  network     = "default"
+  project     = var.project_id
+  dns_logging = true
 }
 
 
-resource "google_compute_subnetwork" "default_subnets" {
-  for_each     = toset(var.default_subnets)
-  name         = "default"
-  project      = var.project_id
-  ip_cidr_range = "10.10.0.0/20"
-  network      = "default"
-  region       = each.key
+resource "google_compute_subnetwork" "default" {
+  provider = google.gcp
+  for_each = toset(var.regions)
+  name                     = "default"
+  ip_cidr_range            = "10.128.0.0/20"
+  network                  = "default"
+  project                  = var.project_id
+  region                   = each.key
+  private_ip_google_access = true
 
   log_config {
     aggregation_interval = "INTERVAL_5_SEC"
@@ -47,19 +64,3 @@ resource "google_compute_subnetwork" "default_subnets" {
     metadata             = "INCLUDE_ALL_METADATA"
   }
 }
-
-resource "google_compute_project_metadata" "oslogin" {
-  project = var.project_id
-
-  metadata = {
-    enable-oslogin = "TRUE"
-  }
-}
-
-resource "google_compute_global_network_endpoint_group" "default" {
-    name  = "default-global-neg"
-    project = var.project_id
-    network = "default"
-    default_port = 80
-}
-
