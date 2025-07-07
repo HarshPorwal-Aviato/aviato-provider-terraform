f88d4f2e3da780dc9015937a34b2ef60a08e78c7
HarshPorwal-Aviato <harsh@aviato.consulting>
2025-07-07 16:38:21 +0530

    Initial commit

--- output\iam.tf
+++ output\iam.tf
@@ -1,18 +1,38 @@
-resource "google_project_iam_member" "cloud_asset" {
+resource "google_project_iam_member" "cloud_asset_inventory" {
+  provider = google.gcp
   project = var.project_id
-  role    = "roles/cloudasset.serviceAgent"
-  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-cloudasset.iam.gserviceaccount.com"
+  role   = "roles/cloudasset.owner"
+  member = "user:admin@example.com"
 }
 
-data "google_project" "project" {
-  project_id = var.project_id
+resource "google_project_iam_binding" "no_service_account_user" {
+  provider = google.gcp
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+
+  members = []
 }
 
-resource "google_organization_iam_binding" "org_level_binding" {
-  org_id = "424242424242"
-  role = "roles/viewer"
+resource "google_project_iam_binding" "no_service_account_token_creator" {
+  provider = google.gcp
+  project = var.project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
 
+  members = []
+}
+
+resource "google_project_iam_binding" "remove_admin_privileges" {
+  provider = google.gcp
+  for_each = toset(var.service_accounts_to_remove_admin_privileges)
+  project = var.project_id
+  role = "roles/owner"
   members = [
-    "user:admin@example.com",
+    "serviceAccount:${each.key}"
   ]
+
+  lifecycle {
+    ignore_changes = [
+      members,
+    ]
+  }
 }
