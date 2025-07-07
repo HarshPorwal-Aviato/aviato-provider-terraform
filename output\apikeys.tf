b8b70a643459ec87a7d9a8a63873aabcd1464ac8
HarshPorwal-Aviato <harsh@aviato.consulting>
2025-07-07 18:23:17 +0530

    Initial commit

--- output\apikeys.tf
+++ output\apikeys.tf
@@ -1,6 +1,5 @@
-resource "google_project_iam_member" "api_keys_admin" {
-  provider = google.gcp
+resource "google_project_iam_member" "api_keys_disable" {
   project = var.project_id
-  role   = "roles/apikeys.admin"
-  member = "user:admin@example.com"
+  role    = "roles/apikeys.admin"
+  member  = "allUsers"
 }
