a2014a5b493bd58168fd62df18b91dc6fa15f0d8
HarshPorwal-Aviato <harsh@aviato.consulting>
2025-07-07 12:37:51 +0530

    Initial commit

--- output/variables.tf
+++ output/variables.tf
@@ -1,58 +1,77 @@
 variable "project_id" {
-  type        = string
-  description = "The ID of the project"
-  default     = "aviato-game-fight-rvxirf"
+  type = string
+  default = "aviato-game-fight-rvxirf"
 }
 
-variable "location" {
-  type        = string
-  description = "The location where resources will be created"
-  default     = "US"
-}
-
-variable "default_vpc_regions" {
+variable "regions" {
   type = list(string)
   default = [
-    "us-west1",
-    "us-west2",
-    "us-west3",
-    "us-west4",
-    "us-central1",
-    "us-east1",
-    "us-east4",
-    "us-south1",
-    "northamerica-northeast1",
-    "northamerica-northeast2",
-    "southamerica-east1",
+    "africa-south1",
     "southamerica-west1",
-    "europe-west1",
-    "europe-west2",
-    "europe-west3",
-    "europe-west4",
-    "europe-west6",
-    "europe-west8",
-    "europe-west9",
-    "europe-west10",
-    "europe-west12",
-    "europe-north1",
-    "europe-north2",
-    "europe-central2",
-    "europe-southwest1",
-    "asia-east1",
+    "us-west4",
+    "me-central2",
     "asia-east2",
     "asia-northeast1",
-    "asia-northeast2",
-    "asia-northeast3",
-    "asia-southeast1",
-    "asia-southeast2",
-    "asia-south1",
     "asia-south2",
+    "asia-northeast3",
+    "us-south1",
     "australia-southeast1",
+    "us-east4",
+    "us-west3",
+    "asia-southeast1",
+    "europe-southwest1",
+    "asia-east1",
+    "europe-north2",
     "australia-southeast2",
-    "africa-south1",
+    "northamerica-northeast2",
+    "asia-southeast2",
+    "northamerica-northeast1",
+    "asia-south1",
+    "europe-west6",
+    "europe-west1",
+    "southamerica-east1",
+    "asia-northeast2",
+    "europe-north1",
+    "us-east5",
+    "us-west2",
+    "us-west1",
+    "europe-west10",
+    "us-central1",
+    "northamerica-south1",
+    "europe-west4",
+    "europe-west9",
+    "europe-west2",
+    "europe-west12",
+    "europe-west8",
     "me-central1",
-    "me-central2",
+    "europe-west3",
+    "us-east1",
+    "europe-central2",
     "me-west1"
   ]
-  description = "list of regions where default subnets exits"
 }
+
+variable "default_network_name" {
+  type    = string
+  default = "default"
+}
+
+variable "default_allow_ssh_firewall_name" {
+  type = string
+  default = "default-allow-ssh"
+}
+
+variable "default_allow_rdp_firewall_name" {
+  type = string
+  default = "default-allow-rdp"
+}
+
+variable "bucket_names" {
+  type = list(string)
+  default = [
+    "aviato-game-fight-rvxirf.appspot.com",
+    "aviato-game-fight-rvxirf_bucket",
+    "staging.aviato-game-fight-rvxirf.appspot.com"
+  ]
+}
+
