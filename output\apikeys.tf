4ae6c01e3ed2c56f5c5e8648945500df09574447
HarshPorwal-Aviato <harsh@aviato.consulting>
2025-07-07 16:38:08 +0530

    Initial commit

--- output\apikeys.tf
+++ output\apikeys.tf
@@ -1,17 +1,6 @@
 resource "google_project_iam_member" "api_keys_admin" {
+  provider = google.gcp
   project = var.project_id
-  role    = "roles/apikeys.admin"
-  member  = "user:test@example.com"
-}
-
-resource "google_apikeys_key" "api_key" {
-  name       = "api-key"
-  project = var.project_id
-
-  restrictions {
-    api_targets {
-      service = "translate.googleapis.com"
-      methods = ["translate"]
-    }
-  }
+  role   = "roles/apikeys.admin"
+  member = "user:admin@example.com"
 }
