345710056b01a98c7610b2218c4542425abb6ac4
HarshPorwal-Aviato <harsh@aviato.consulting>
2025-07-10 12:32:59 +0530

    Initial commit

---
+++ output\variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  type    = string
+  default = "aviato-game-fight-rvxirf"
+}
+
+variable "bucket_name" {
+  type = string
+  default = "gcp-logging-bucket-tf"
+}
+
+variable "region" {
+  type = string
+  default = "us-central1"
+}
