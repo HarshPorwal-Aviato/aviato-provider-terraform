cda413183aa579e5ba235da548564e3324cbd6e8
HarshPorwal-Aviato <harsh@aviato.consulting>
2025-07-11 05:40:37 +0530

    Initial commit

---
+++ output/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  type    = string
+  default = "aviato-game-fight-rvxirf"
+}
+
+variable "log_bucket_name" {
+  type    = string
+  default = "aviato-game-fight-rvxirf-logs"
+}
